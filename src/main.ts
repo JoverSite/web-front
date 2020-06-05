56c14d6f452fea1828f7ec64a7ddd632459a5238
JoverZhang <joverzh@gmail.com>
2020-06-05 22:18:53 +0800

    chore(serviceWorker): remove service worker.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from '@/router'
 import store from '@/store'
 import vuetify from '@/plugins/vuetify'
 
-import '@/registerServiceWorker'
+// import '@/registerServiceWorker'
 import '@/assets/styles/index.scss'
 
 Vue.config.productionTip = false
